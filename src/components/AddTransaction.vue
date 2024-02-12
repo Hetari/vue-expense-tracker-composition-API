d6d3cfd75310710bdd78ef2cc7b90a1cc8b45e8f
Hetari <hetari4py@gmail.com>
2024-02-12 16:10:43 +0300

    Finish the vue-toastification and the transition submit

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,21 +1,62 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input v-model="text" type="text" id="text" placeholder="Enter text..." />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="text" id="amount" placeholder="Enter amount..." />
+      <input
+        v-model="amount"
+        type="text"
+        id="amount"
+        placeholder="Enter amount..."
+      />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+const text = ref("");
+const amount = ref("");
+const emit = defineEmits(["AddTransaction"]);
+
+function isNumber(value) {
+  if (typeof value !== "number") {
+    return false;
+  }
+
+  return !isNaN(value) && Number.isFinite(value);
+}
+
+const onSubmit = () => {
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    toast.error("Please fill in all fields");
+    return;
+  }
+
+  if (!isNumber(amount.value)) {
+    toast.warning("You should write a valid number in the amount filed!");
+    amount.value = "";
+    return;
+  }
+
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  };
+
+  emit("AddTransaction", transactionData);
+  text.value = "";
+  amount.value = "";
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import App from "./App.vue";
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Toast);
+app.mount("#app");
