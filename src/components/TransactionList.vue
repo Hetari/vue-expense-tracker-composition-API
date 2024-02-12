53b014a0499ec47eb3bced8855d34de06e933147
Hetari <hetari4py@gmail.com>
2024-02-12 15:36:10 +0300

    Finish the transitionList component

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,19 +1,25 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      transaction text
-      <span>-$400.0 </span>
-      <button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      transaction text
-      <span>$0.0 </span>
+    <li
+      v-for="transaction in transitions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }}
+      <span>{{ transaction.amount }}</span>
       <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transitions: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
